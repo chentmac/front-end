47dad0e01e9ff307836382b0cda1db4b3021b4a7
Levinzhong <Levin.zhong@oocl.com>
2019-03-11 20:21:29 +0800

    generate unit test

---
+++ back-end/src/test/java/com/oocl/todolist/service/impl/TaskServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.oocl.todolist.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskServiceImplTest {
+
+  @Test
+  public void name() {
+
+  }
+}
---
+++ back-end/src/test/java/com/oocl/todolist/service/impl/UserServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.oocl.todolist.service.impl;
+
+import static org.junit.Assert.*;
+
+public class UserServiceImplTest {
+
+}
