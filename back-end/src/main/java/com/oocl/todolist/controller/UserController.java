58efb143e34b62396640d7c89347a5ef4e9a88cf
Amelia <amelia.cao@oocl.com>
2019-03-11 21:28:59 +0800

    finish login and register.

--- back-end/src/main/java/com/oocl/todolist/controller/UserController.java
+++ back-end/src/main/java/com/oocl/todolist/controller/UserController.java
@@ -1,16 +1,16 @@
 package com.oocl.todolist.controller;
 
-import com.oocl.todolist.entity.User;
 import com.oocl.todolist.service.UserService;
 import com.oocl.todolist.vo.LoginVo;
 import com.oocl.todolist.vo.UserVo;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -19,15 +19,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/findAll")
+    @GetMapping(value = "/")
     public List<UserVo> findAll() {
         return userService.getAllUserInfo();
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping(value = "")
     public boolean save(@RequestBody UserVo userVo) {
-        boolean result = userService.save(userVo);
-        return result;
+        return userService.save(userVo);
+
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
