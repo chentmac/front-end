826662dd881ab85b013972573a0c59d6471294c0
Levinzhong <Levin.zhong@oocl.com>
2019-03-22 19:53:18 +0800

    add zuul

--- back-end/src/main/java/com/oocl/todolist/TodolistApplication.java
+++ back-end/src/main/java/com/oocl/todolist/TodolistApplication.java
@@ -2,7 +2,9 @@ package com.oocl.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
