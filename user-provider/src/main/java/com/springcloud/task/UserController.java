452584bda98c9987f3f9ec12a0a1ad5400ccc8f9
Levinzhong <Levin.zhong@oocl.com>
2019-03-25 21:15:08 +0800

    add spring cloud11

--- user-provider/src/main/java/com/springcloud/task/UserController.java
+++ user-provider/src/main/java/com/springcloud/task/UserController.java
@@ -1,5 +1,6 @@
 package com.springcloud.task;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,27 +10,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private UserService userService;
 
-    @GetMapping
-    public List<UserVo> findAll() {
-        return userService.getAllUserInfo();
+  @GetMapping
+  public List<UserVo> findAll() {
+    return userService.getAllUserInfo();
+  }
+
+  @PostMapping
+  public boolean save(@RequestBody UserVo userVo) {
+    return userService.save(userVo);
+
+  }
+
+  @HystrixCommand(fallbackMethod = "loginFallback")
+  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  public User login(@RequestBody LoginVo loginVo) {
+
+    User user = userService.login(loginVo);
+    if(user == null){
+      throw new RuntimeException("fail");
     }
+    return user;
+  }
 
-    @PostMapping
-    public boolean save(@RequestBody UserVo userVo) {
-        return userService.save(userVo);
-
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public User login(@RequestBody LoginVo loginVo) {
-        return userService.login(loginVo);
-    }
+  public User loginFallback(@RequestBody LoginVo loginVo) {
+    return new User();
+  }
 
 }
