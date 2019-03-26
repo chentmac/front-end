1c6150988965541886eb79f6eb21492934bbe97b
CHENJA25 <jason.chen@oocl.com>
2019-03-26 19:54:20 +0800

    update

---
+++ config-client/src/main/java/com/oocl/configclient/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.oocl.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //开启更新功能
+@RequestMapping("api")
+public class TestController {
+
+  @Value("${spring.application.name}")
+  private String fromValue;
+
+  /**
+   * 返回配置文件中的值
+   */
+  @GetMapping("/from")
+  @ResponseBody
+  public String returnFormValue(){
+    return fromValue;
+  }
+}
