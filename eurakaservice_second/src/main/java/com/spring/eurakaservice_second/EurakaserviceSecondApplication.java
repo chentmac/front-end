e6f695571b71910b51f338cd41dd853ec4a1f3a4
ZHENGDU <dunn.zheng@oocl.com>
2019-03-27 15:45:39 +0800

    euraka cluster

--- eurakaservice_second/src/main/java/com/spring/eurakaservice_second/EurakaserviceSecondApplication.java
+++ eurakaservice_second/src/main/java/com/spring/eurakaservice_second/EurakaserviceSecondApplication.java
@@ -2,8 +2,10 @@ package com.spring.eurakaservice_second;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurakaserviceSecondApplication {
 
   public static void main(String[] args) {
