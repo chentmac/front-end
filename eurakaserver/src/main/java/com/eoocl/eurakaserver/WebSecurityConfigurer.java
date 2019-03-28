86f9a5213bfa113f14ccb5b533ddf565ec828b3b
Amelia <amelia.cao@oocl.com>
2019-03-28 23:23:01 +0800

    euraka security.

---
+++ eurakaserver/src/main/java/com/eoocl/eurakaserver/WebSecurityConfigurer.java
@@ -0,0 +1,31 @@
+package com.eoocl.eurakaserver;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+    super.configure(http);
+  }
+
+  @Bean
+  public UserDetailsService userDetailService() {
+    User.UserBuilder builder = User.withDefaultPasswordEncoder();
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+    // 必须添加roles,否则报错
+    manager.createUser(builder.username("root").password("123456").roles("USER").build());
+
+    return manager;
+  }
+}
